7937444044a021c570fec4aec872dfff4bbce8cb
Rachana <rachanaraju03@gmail.com>
2024-08-03 22:25:02 -0700

    changed firebase config

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import { config } from 'dotenv'
+
+config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvICdh91jhWj6iU97haasKQgy7kx-YhM4",
-  authDomain: "inventory-management-7d13c.firebaseapp.com",
-  projectId: "inventory-management-7d13c",
-  storageBucket: "inventory-management-7d13c.appspot.com",
-  messagingSenderId: "843508515771",
-  appId: "1:843508515771:web:ad00ac18e4c67843763a9e",
-  measurementId: "G-FJQ24YWCSB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
