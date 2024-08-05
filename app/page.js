8069809fffee6e9b16a7c6d19a87892ebb16075e
Rachana <rachanaraju03@gmail.com>
2024-08-04 22:57:28 -0700

    edited styling of page

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import {useState, useEffect} from 'react'
 import { firestore } from '@/firebase'
 import {Box, Stack, Typography, Modal, TextField, Button} from '@mui/material'
 import { collection, deleteDoc, getDocs, query, setDoc, doc, getDoc} from 'firebase/firestore'
-
-const item = ['tomato','potato', 'onion', 'garlic', 'ginger','carrot']
+import SearchIcon from '@mui/icons-material/Search'
+import InputAdornment from '@mui/material/InputAdornment';
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -64,40 +64,60 @@ export default function Home() {
   const handleClose = () => setOpen(false)
 
   return (
-  <Box width='100vw' height='100vh' display='flex' flexDirection='column' justifyContent='center' alignItems='center' gap={2}>
-    <Modal open={open} onClose={handleClose}>
-      <Box position='absolute' top='50%' left='50%' width={400} bgcolor='white' border='2px solid #000' boxShadow={24} p={4} display='flex' flexDirection='column' gap={3} sx={{transform:"translate(-50%,-50%)"}}>
-        <Typography variant='h6'>Add Item</Typography>
-        <Stack width='100%' direction='row' spacing={2}>
-          <TextField variant='outlined' fullWidth value={itemName} onChange={(e)=>{setItemName(e.target.value)}}/>
-          <Button variant='outlined' onClick={() =>{
-            addItem(itemName)
-            setItemName('')
-            handleClose()}}>Add</Button>
-        </Stack>
+    <Box width='100vw' height='100vh' display='flex' flexDirection='column'  alignItems='center' gap={2} bgcolor='#EADDCA'>
+      <Box width='100vw' justifyContent='center' alignContent='center' alignItems='center' position='relative'>
+        <Box width='100vw' height='40vh' overflow='hidden' display='flex' flexDirection='column' justifyContent='center' alignItems='center' margin='auto' position='relative'> 
+          <img width='100%' src='https://images.unsplash.com/photo-1569717315843-b6a4c2c67ead?q=80&w=3029&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+        </Box>
+        <Typography variant='h1' position='absolute' left='50%' top='50%' sx={{transform:"translate(-50%,-50%)"}} bgcolor='#C19A6B'>PantryPal</Typography>
+        <Typography variant='h6' position='absolute' left='50%' top='50%' sx={{transform:"translate(-50%,300%)"}} bgcolor='#C19A6B'>Manage your kitchen inventory with this tracker!</Typography>
       </Box>
-    </Modal>
-    <Button variant='contained' onClick={()=>{
-      handleOpen()
-    }}>Add New Item</Button>
-    <Box border={'1px solid #333'}>
-      <Box width='800px' height='100px' bgcolor='#ADD8E6'>
-        <Typography variant='h2' color='#333' display='flex' alignItems='center' justifyContent='center'>Inventory Items</Typography>
-      </Box>
-    <Stack width='800px' height='300px' spacing={2} overflow='auto'>
-      {
-        inventory.map(({name, quantity}) => (
-          <Box key={name} width='100%' minHeight='150px' display='flex' alignItems='center' justifyContent='space-between' bgcolor='#f0f0f0' padding={5}>
-            <Typography variant='h3' color='#333' textAlign='center'>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
-            <Typography variant='h3' color='#333' textAlign='center'>{quantity}</Typography>
-            <Stack direction='row' spacing={2}>
-              <Button variant='contained' onClick={() => addItem(name)}>Add</Button>
-              <Button variant='contained' onClick={() => removeItem(name)}>Remove</Button>
+      {/* <Box display='flex' justifyContent='center' alignItems='center' >
+        <Typography>Manage your pantry inventory with PantryPal!</Typography>
+      </Box> */}
+      <Box display='flex' flexDirection='column'  alignItems='center' gap={2} margin='auto'>
+      <Box display='flex' flexDirection='row' position='relative' >
+        <Modal open={open} onClose={handleClose}>
+          <Box position='absolute' top='50%' left='50%' width={400} bgcolor='white' border='2px solid #000' boxShadow={24} p={4} display='flex' flexDirection='row' gap={3} sx={{transform:"translate(-50%,-50%)"}}>
+            <Typography variant='h6'>Add Item</Typography>
+            <Stack width='100%' direction='row' spacing={2}>
+              <TextField variant='outlined' fullWidth value={itemName} onChange={(e)=>{setItemName(e.target.value)}}/>
+              <Button variant='outlined' onClick={() =>{
+                addItem(itemName)
+                setItemName('')
+                handleClose()}}>Add</Button>
             </Stack>
           </Box>
-        ))
-      }
-    </Stack>
+        </Modal>
+      <Button sx={{transform:"translate(50%,0%)"}} variant='contained' onClick={()=>{
+        handleOpen()
+      }}>Add New Item</Button>
+      <TextField position='absolute' top='50%' left='50%' width={400} bgcolor='white' border='2px solid #000' boxShadow={24} p={4} display='flex' flexDirection='row' gap={3} sx={{transform:"translate(100%,0%)"}} id="input-with-icon-adornment" label="Search Inventory" variant="standard" InputProps={{endAdornment:(
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+          )}}/>
+      </Box>
+      <Box border={'1px solid #333'}>
+        <Box width='800px' height='40px' bgcolor='#ADD8E6'>
+          <Typography variant='h4' color='#0096FF' display='flex' alignItems='center' justifyContent='center'>Items</Typography>
+        </Box>
+      <Stack width='800px' height='180px' spacing={2} overflow='auto' >
+        {
+          inventory.map(({name, quantity}) => (
+            <Box key={name} width='100%' height='30px' display='flex' alignItems='center' justifyContent='space-between' bgcolor='#F0FFFF' padding={3}>
+              <Typography variant='h6' color='#0096FF' textAlign='center'>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+              <Typography variant='h6' color='#0096FF' textAlign='center'>{quantity}</Typography>
+              <Stack direction='row' spacing={2}>
+                <Button size='small' variant='contained' onClick={() => addItem(name)}>Add</Button>
+                <Button size='small' variant='contained' onClick={() => removeItem(name)}>Remove</Button>
+              </Stack>
+            </Box>
+          ))
+        }
+      </Stack>
+      </Box>
     </Box>
-  </Box>)
+  </Box>
+  )
 }
